17bb80eae2596bfd17d1e80b2a2962f4ab025957
nihao151 <2443743398@qq.com>
2025-12-05 21:10:10 +0800

    在hive中建立house_info表

---
+++ Data-analysis/src/main/resources/house_info.sql
@@ -0,0 +1,83 @@
+CREATE database dataanalysis;
+use dataanalysis;
+--建立初始表
+CREATE TABLE house_info (
+    district STRING COMMENT '市区',
+    community STRING COMMENT '小区',
+    layout STRING COMMENT '户型',
+    orientation STRING COMMENT '朝向',
+    floor_num INT COMMENT '楼层',  -- 改为floor_num
+    decoration STRING COMMENT '装修情况',
+    elevator STRING COMMENT '电梯',
+    area INT COMMENT '面积(㎡)',
+    price INT COMMENT '价格(万元)',
+    build_year STRING COMMENT '年份'
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+TBLPROPERTIES ("skip.header.line.count"="1");
+--插入数据
+LOAD DATA INPATH '/user/master/dataanalysis/data.csv' 
+OVERWRITE INTO TABLE house_info;
+--建立清理后的表
+CREATE TABLE IF NOT EXISTS house_info_clean (
+    rowkey STRING COMMENT '唯一标识:市区_小区_序号',
+    district STRING COMMENT '市区',
+    community STRING COMMENT '小区',
+    layout STRING COMMENT '户型',
+    orientation STRING COMMENT '朝向',
+    floor_num INT COMMENT '楼层',
+    decoration STRING COMMENT '装修情况',
+    elevator_int INT COMMENT '电梯:1有/0无',
+    area INT COMMENT '面积(㎡)',
+    price INT COMMENT '价格(万元)',
+    price_per_sqm INT COMMENT '单价(元/㎡)',
+    build_year STRING COMMENT '建造年份',
+    house_age INT COMMENT '房龄(年)'
+)
+STORED AS ORC
+TBLPROPERTIES ("orc.compress"="SNAPPY");
+--插入清理后的数据
+INSERT OVERWRITE TABLE house_info_clean
+SELECT 
+    -- 生成rowkey（去掉特殊字符）
+    CONCAT(district, '_', 
+           regexp_replace(community, '[^\\w\\u4e00-\\u9fff]', '_'), 
+           '_', 
+           cast(row_number() OVER (ORDER BY district, community) as string)) as rowkey,
+    
+    -- 原始字段
+    district,
+    community,
+    layout,
+    orientation,
+    floor_num,
+    decoration,
+    
+    -- 电梯字段转换
+    CASE 
+        WHEN elevator = '有电梯' THEN 1
+        WHEN elevator = '无电梯' THEN 0
+        ELSE 0
+    END as elevator_int,
+    
+    area,
+    price,
+    
+    -- 单价取整（万元/㎡）
+    CAST(ROUND(price * 10000.0 / area) as INT) as price_per_sqm,
+    
+    build_year,
+    
+    -- 计算房龄（假设当前年份为2024）
+    (2024 - CAST(build_year AS INT)) as house_age
+    
+FROM house_info
+WHERE district IS NOT NULL 
+  AND community IS NOT NULL 
+  AND area > 0 
+  AND price > 0
+  AND build_year REGEXP '^[0-9]{4}$';
+
+
---
+++ Data-analysis/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 24437
+  Date: 2025/12/5
+  Time: 下午8:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
