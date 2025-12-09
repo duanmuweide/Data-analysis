103d29f398b38aa3925afb04b5869a3bd05b8b0f
陈政铭 <yinxiamengjiang@qq.com>
2025-12-09 11:56:58 +0800

    提交自定义函数的查询的所用的mysql建表文件

---
+++ nutrient_surplus_atm_dep_corr.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80021
+ Source Host           : localhost:3306
+ Source Schema         : american_data_analysis
+
+ Target Server Type    : MySQL
+ Target Server Version : 80021
+ File Encoding         : 65001
+
+ Date: 09/12/2025 11:55:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for nutrient_surplus_atm_dep_corr
+-- ----------------------------
+DROP TABLE IF EXISTS `nutrient_surplus_atm_dep_corr`;
+CREATE TABLE `nutrient_surplus_atm_dep_corr`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `year` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avg_n_ag_surplus` decimal(20, 6) NOT NULL,
+  `avg_n_atm_dep` decimal(20, 6) NOT NULL,
+  `corr_coefficient` decimal(20, 6) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
